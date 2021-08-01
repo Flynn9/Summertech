fd49e2ba8b5d2e78243d43226b1687a1c9c64a8c
User <User@10.136.128.15>
2021-08-01 17:03:50 -0400

    H

---
+++ src/Calculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Calculator {
+
+	public static void main(String[] args) {
+		boolean r = true;
+		while (r==true){
+		System.out.println("Choose a number-I will come to your house and eliminate you if you enter something else");
+		Scanner query = new Scanner(System.in);
+		int num1 = query.nextInt();
+		System.out.println("choose an operation between +.-,x,/");
+		String operation = query.next();
+		System.out.println("Choose another number- I will come to your house and eliminate you if you enter something else");
+		int num2 = query.nextInt();
+		if (operation.equals("x")) {
+			System.out.println(num1 * num2);
+		}
+		else if (operation.equals("/")) {
+			System.out.println(num1 / num2);
+		}
+		else if (operation.equals("+")) {
+			System.out.println(num1 + num2);
+		}
+		else if (operation.equals("-")) {
+			System.out.println(num1 - num2);
+		}
+		else {
+			System.out.println("You entered in an invalid operation, please restart!");
+		}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
---
+++ src/helloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class helloWorld {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String myAnswer = s.nextLine();
+		System.out.print(myAnswer);
+		// TODO Auto-generated method stub
+
+	}
+
+}
