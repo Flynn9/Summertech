530db089b92c0c0ceb5c00c51e5d0caf61457f7e
User <User@10.136.128.15>
2021-08-06 09:57:37 -0400

    El momento de h

--- src/blackJack/Deck.java
+++ src/blackJack/Deck.java
@@ -12,6 +12,16 @@ public class Deck {
 			for (Card.cardSuit suitGen: Card.cardSuit.values()) {
 				cardList.add(new Card(valueGen, suitGen));
 			}
+			}
+		}
+	public Deck(boolean emptydeck) {
+		emptydeck = false;
+		if (emptydeck==false){
+		for (int valueGen = 1;valueGen<14;valueGen = valueGen+1) {
+			for (Card.cardSuit suitGen: Card.cardSuit.values()) {
+				cardList.add(new Card(valueGen, suitGen));
+			}
+			}
 		}
 	}
 	@Override
@@ -27,8 +37,21 @@ public class Deck {
 		for (int x=0;x<cardList.size()*4;x++) {
 			Card temp = cardList.remove(0);
 			cardList.add(r.nextInt(cardList.size()), temp);
-
 		}
 		return this;
 	}	
+	public Card pickCard() {
+		Card playerCard = cardList.remove(0);
+		return playerCard;
+	}
+	public Card pickCard(int index) {
+		Card playerCard = cardList.remove(index);
+		return playerCard;
+	}
+	public void addCard(Card playerCard) {
+		cardList.add(playerCard);
+	}
+	public void addCard(Card playerCard, int index) {
+		cardList.add(index, playerCard);
+	}
 }
--- src/blackJack/blackJack.java
+++ src/blackJack/blackJack.java
@@ -1,15 +1,17 @@
 
 package blackJack;
+import java.util.Scanner;
 import java.util.Random;
 import blackJack.Card.cardSuit;
 
 public class blackJack {
 
 	public static void main(String[] args) {
-		Deck Deck1 = new Deck ();
+		Deck Deck1 = new Deck();
 		Deck1.shuffleDeck();
-		System.out.println(Deck1);
-
+		
+		System.out.println(Deck1.pickCard());
+		System.out.println("do you want to get another card?");
 
 	}
 }
